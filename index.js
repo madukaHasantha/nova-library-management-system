90ef9d6659151ba204317056267108f81512ec06
madukaHasantha <77095852+madukaHasantha@users.noreply.github.com>
2024-05-12 18:07:50 +0530

    change port

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const bookRoutes = require('./routes/bookRoutes');
 const borrowingRoutes = require('./routes/borrowingRoutes');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 app.use(bodyParser.json());
 
